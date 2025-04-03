121a7b9560fd5c5bf65f1a10b2dfb506343e71a1
João Marcos Coutinho Nunes <jmcoutinhonunes@gmail.com>
2025-04-03 17:17:43 -0300

    feat: add connection to API

--- src/actions/chat/sendReply.ts
+++ src/actions/chat/sendReply.ts
@@ -3,15 +3,45 @@
 import { MessageType, SentByEnum } from "@/types/MessageType";
 import { generateRandomString } from "@/helpers/generateRandomString";
 import updateChat from "./updateChat";
+import readChatByID from "./readChat";
 
+
+const BASE_URL = process.env.DBSTRUCTUREAPI_HOST || "http://127.0.0.1:8002/"
+const API_KEY = process.env.DBSTRUCTUREAPI_API_KEY as string
+const DB_ID = "67bf98cc596db49beda4af2a"
 
 export default async function sendReply(chaID: string) {
     try {
+        let content: string = ""
+
         const newID = generateRandomString();
+        const chat = await readChatByID(chaID);
+
+        const userMessage = chat.messages[chat.messages.length - 1].content
+        
+        const response = await fetch(`${BASE_URL}db_structure/${DB_ID}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X_API_KEY": API_KEY
+            },
+            body: JSON.stringify({
+                "db_id": DB_ID,
+                "db_name": "teste",
+                "order": userMessage
+            })
+        })
+
+        if (!response.ok) {
+            content = "Parece que aconteceu algum erro!"
+        } else {
+            const data = await response.json();
+            content = data.response;
+        }
 
         let reply: MessageType = {
             id: newID,
-            content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam commodi, rem veniam amet, corrupti, excepturi iusto rerum doloribus numquam pariatur repudiandae illo ullam sapiente reiciendis voluptatem aspernatur beatae iure? Tempora!",
+            content: content,
             sentBy: SentByEnum.BOT
         }
 
